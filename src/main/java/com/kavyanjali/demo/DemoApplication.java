e60b532c12981accf052a15b170d9a2830fdd2a8
Kavyanjali07 <kavyanjalivashishtha@gmail.com>
2026-07-13 11:05:06 +0530

    Student Class

---
+++ src/main/java/com/kavyanjali/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.kavyanjali.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		Student student = new Student(22,"Kavya");
+
+		System.out.println(student.getAge());
+		System.out.println(student.getName());
+
+	}
+
+}
