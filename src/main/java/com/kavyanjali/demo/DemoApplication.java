91c0bbc8b4c9d95dde50e8163d9bd7994ffbd69c
Kavyanjali07 <kavyanjalivashishtha@gmail.com>
2026-07-14 02:41:38 +0530

    Dependency Injection

--- src/main/java/com/kavyanjali/demo/DemoApplication.java
+++ src/main/java/com/kavyanjali/demo/DemoApplication.java
@@ -1,5 +1,5 @@
 package com.kavyanjali.demo;
-
+import com.kavyanjali.demo.DependencyInjection.OrderService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,11 +9,11 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-		Student student = context.getBean(Student.class);
-		student.setAge(22);
-		student.setName("Harsh");
-		System.out.println(student.getAge());
-		System.out.println(student.getName());
+//		Student student = context.getBean(Student.class);
+//		student.setAge(22);
+//		student.setName("Harsh");
+//		System.out.println(student.getAge());
+//		System.out.println(student.getName());
 //		Student student = new Student(22,"Kavya");
 //
 //		System.out.println(student.getAge());
